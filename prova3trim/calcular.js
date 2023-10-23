c7b39fbbaf0c7c033e9e16c3e85a7bd19e9f31c6
elliott <130366417+toorques@users.noreply.github.com>
2023-10-23 11:16:48 -0300

    Add files via upload

---
+++ prova3trim/calcular.js
@@ -0,0 +1,14 @@
+function Calcular(){
+
+    let idadeCanina = prompt ("Qual a idade de seu cachorro?");
+
+    let resultado = idadeCanina * 7;
+
+
+    let mostraResultado = document.querySelector(".mostraResultado"); 
+
+    mostraResultado.textContent = "Resultado: a idade humana do seu cachorro é " + resultado;
+    // BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    // POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING 
+    // oh céus!
+}
